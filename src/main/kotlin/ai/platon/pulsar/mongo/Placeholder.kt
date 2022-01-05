d35ffb60414159d38b4893c2a4842f23c18d257f
Vincent Zhang <ivincent.zhang@gmail.com>
2022-01-05 13:35:19 +0800

    first commit

---
+++ src/main/kotlin/ai/platon/pulsar/mongo/Placeholder.kt
@@ -0,0 +1,8 @@
+package ai.platon.pulsar.mongo
+
+/**
+ *
+ * Since gora-0.x requires mongo-java-driver while spring-boot-2.4.x does not support it,
+ * we make a shaded jar to hide mongo-java-driver.
+ * */
+class Placeholder
